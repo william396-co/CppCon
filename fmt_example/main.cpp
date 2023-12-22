0d1c9135962c0ebb87964fa962c2bb9788e01f10
huiping.liu <huiping.liu@silverkeyz.com>
2023-12-22 15:35:55 +0800

    fmt example(include user type specialization)

---
+++ fmt_example/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <sstream>
+#include <format>
+#include <vector>
+
+// 节点类型
+enum class HostType
+{
+    None = 0,   // 非法的主机节点
+    Gate = 1,   // 网关服务器
+    Scene = 2,  // 场景服务器
+    World = 3,  // 世界服务器
+    Copy = 4,   // 副本服务器
+    Logger = 5, // 日志服务器
+    Data = 6,   // 数据服务器
+    Login = 7,  // 验证服务器
+    Master = 8, // 管理服务器
+    // TODO: ....
+    Global = 11,  // 全局服务器
+    Transit = 12, // 中转服务器
+    Cross = 13,   // 跨服服务器
+    Any = 255,    // 任何节点
+};
+
+typedef std::vector<HostType> HostTypes; // 节点类型列表
+
+template<> struct std::formatter<HostTypes>
+{
+    template<typename ParseContext>
+    constexpr auto parse( ParseContext & pc )
+        -> decltype( pc.begin() )
+    {
+        return pc.begin();
+    }
+    template<typename FormatContext>
+    auto format( const HostTypes & values, FormatContext & fc ) const -> decltype( fc.out() )
+    {
+        std::ostringstream out;
+        for ( auto i = 0; i != values.size(); ++i ) {
+            out << (int32_t)values[i];
+            if ( i != values.size() - 1 ) {
+                out << ",";
+            }
+        }
+        return std::format_to( fc.out(), "[{}]", out.str() );
+    }
+};
+
+template<> struct std::formatter<HostType>
+{
+    template<typename ParseContext>
+    constexpr auto parse( ParseContext & pc ) -> decltype( pc.begin() )
+    {
+        return pc.begin();
+    }
+
+    template<typename FormatContext>
+    auto format( const HostType & value, FormatContext & fc ) const -> decltype( fc.out() )
+    {
+        return std::format_to( fc.out(), "{}", (int32_t)value );
+    }
+};
+
+int main()
+{
+
+    HostType ht = HostType::Master;
+    std::cout << std::format( "ht:{}\n", ht );
+
+    HostTypes hts = { HostType::Master, HostType::Cross };
+    std::cout << std::format( "hts:{}\n", hts );
+
+    return 0;
+}

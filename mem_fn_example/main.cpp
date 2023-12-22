cedba2631eb8e3a685904fecc4ddf2cc31f3a2a6
huiping.liu <huiping.liu@silverkeyz.com>
2023-12-22 15:18:21 +0800

    mem_fn call by null object

---
+++ mem_fn_example/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Test
+{
+public:
+    void print()
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
+
+    virtual void println()
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
+};
+
+int main()
+{
+    //
+    {
+        Test * t = nullptr;
+        t->print();   // ok,like call print(this) ,this not deref
+        t->println(); // segement fault ,this->[0]()println(this)
+    }
+
+    return 0;
+}
